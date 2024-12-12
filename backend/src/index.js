d79573e59a4bde333ac2861bbead05229e776ff6
@tamaghno <tamaghnog@gmail.com>
2024-12-12 21:49:32 +0530

    refactor backend structure: update entry point, add dotenv support, and implement authentication routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,14 @@
 import express from 'express'
-
+import dotenv from 'dotenv'
+import authRoutes from '../routes/auth.route.js';
+dotenv.config();
 const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use("/api/auth", authRoutes)
 
 app.listen(5001, () => {
     console.log("server is running on port 5001");
 })
+
