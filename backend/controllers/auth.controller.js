476c1203f30124f8cf2f70fd7886d868e3452968
@tamaghno <tamaghnog@gmail.com>
2024-12-14 00:01:27 +0530

    implement user authentication: add signup and login functionality, connect to MongoDB, and generate JWT

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,11 +1,59 @@
-export const signup =  (req, res) => {
-   
-}
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
-export const login =  (req, res) => {
+export const signup = async (req, res) => {
+    const { email, fullName, password } = req.body;
+
+    try {
+        if (!email || !fullName || !password) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+
+        // Input validation
+        if (password.length < 6) {
+            return res.status(400).json({ message: "Password must be at least 6 characters long" });
+        }
+
+        // Check existing user
+        const user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({ message: "Email already exists" });
+        }
+
+        // Hash password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // Create new user
+        const newUser = new User({
+            fullName,
+            email,
+            password: hashedPassword
+        });
+
+        // Save user and generate token
+        await newUser.save();
+        generateToken(newUser._id, res);
+
+        // Return user data
+        return res.status(201).json({
+            _id: newUser._id,
+            fullName: newUser.fullName,
+            email: newUser.email,
+            profilePic: newUser.profilePic
+        });
+
+    } catch (error) {
+        console.log("Signup error:", error);
+        res.status(500).json({ message: "Something went wrong" });
+    }
+};
+
+export const login = (req, res) => {
     res.send("login route");
-}
+};
 
-export const logout =  (req, res) => {
+export const logout = (req, res) => {
     res.send("logout route");
-}
+};
