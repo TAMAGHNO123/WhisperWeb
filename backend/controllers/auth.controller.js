883d05922f783c01cc317294fbd84de4fd6fc39b
@tamaghno <tamaghnog@gmail.com>
2024-12-14 21:50:06 +0530

    enhance user profile management: add profile picture upload and user authentication check, implement route protection

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,6 @@
+import cloudinary from "../lib/cloudinary.js";
 import { generateToken } from "../lib/utils.js";
+
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 
@@ -86,4 +88,27 @@ export const logout = (req, res) => {
     console.log("Error in logout controller:", error.message);
     res.status(500).json({ message: "Internal server error" });
    }
+};
+
+export const updateProfile = async (req, res) => {
+    try {
+        const {profilePic} = req.body;
+        const userid = req.user._id;
+
+
+        if (!profilePic) {
+            return res.status(400).json({ message: "Profile picture is required" });
+        }
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic);
+        const updatedUser = await User.findByIdAndUpdate(userid, {profilePic: uploadResponse.secure_url}, {new: true});
+        res.status(200).json({ updatedUser });
+    } catch (error) {
+        console.log("Error in updateProfile controller:", error.message);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+export const checkAuth = (req, res) => {
+    res.status(200).json(req.user);
 };
