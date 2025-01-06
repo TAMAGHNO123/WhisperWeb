7d25611371169bcb3b8827dcefde26ab4e3a1fc5
@tamaghno <tamaghnog@gmail.com>
2025-01-06 21:25:31 +0530

    implement routing and layout: add Navbar and page components; integrate daisyUI for styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+
+   <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/signup' element={<SignUpPage />} />
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/settings' element={<SettingsPage />} />
+      <Route path='/profile' element={<ProfilePage />} />
+      
+   </Routes>
+
+    </div>
   )
 }
 
