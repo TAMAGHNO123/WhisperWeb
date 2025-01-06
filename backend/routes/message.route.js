d1fe7992383fca0fd9ec34a17fd6888260399267
@tamaghno <tamaghnog@gmail.com>
2025-01-06 20:57:09 +0530

    implement messaging functionality: add sendMessage controller and route; update package dependencies

--- backend/routes/message.route.js
+++ backend/routes/message.route.js
@@ -2,10 +2,12 @@ import express from 'express';
 import { protectRoute } from '../middleware/auth.middleware.js';
 import { getUsersSidebar } from '../controllers/message.controller.js';
 import { getMessages } from '../controllers/message.controller.js';
+import { sendMessage } from '../controllers/message.controller.js';
 
 const router = express.Router();
 
 router.get('/users', protectRoute, getUsersSidebar);
 router.get('/:id', protectRoute, getMessages);
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
