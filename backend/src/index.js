3afe138c0a218de198e47984ab1166ec84c7a1a1
@tamaghno <tamaghnog@gmail.com>
2025-01-10 20:52:53 +0530

    implement authentication check: add axios instance, useAuthStore for user state management, and integrate CORS in backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser';
 import authRoutes from '../routes/auth.route.js';
 import { connectDB } from '../lib/db.js';
+import cors from 'cors';
+
 
 
 import messageRoutes from '../routes/message.route.js';
@@ -15,6 +17,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: ["http://localhost:5173"],
+    credentials: true
+}));
 
 app.use("/api/auth", authRoutes)
 app.use("/api/message", messageRoutes)
