4e736d908c951237d6be497e1f889ff8205060c3
@tamaghno <tamaghnog@gmail.com>
2025-01-17 00:27:25 +0530

    add chat-app dependency and configure production static files

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,9 +7,12 @@ import { app, server } from '../lib/socket.js'
 import { connectDB } from '../lib/db.js'
 import cors from 'cors'
 
+import path from 'path'
+
 dotenv.config()
 
 const PORT = process.env.PORT || 5000
+const __dirname = path.resolve();
 
 app.use(express.json())
 app.use(cookieParser())
@@ -20,6 +23,17 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
+
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  })
+}
+
+
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
