ca0dcad77d81cad455d6237a99e64b133e730657
@tamaghno <tamaghnog@gmail.com>
2025-07-17 20:31:23 +0530

    refactor index.js: clean up commented production code for clarity

--- backend/src/index.js
+++ backend/src/index.js
@@ -27,16 +27,20 @@ app.use(cors({
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 
+app.get("/", (req, res) => {
+res.send("WhisperWeb API is running.");
+});
+
 
 // if(process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '/frontend/dist')));
-
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-//   })
-// }
-
-
+  //   app.use(express.static(path.join(__dirname, '/frontend/dist')));
+  
+  //   app.get('*', (req, res) => {
+    //     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    //   })
+    // }
+    
+    
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
